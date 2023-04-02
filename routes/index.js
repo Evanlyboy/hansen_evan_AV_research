0ba0cace17af9559b86aa7ad243714cbe619cfd7
Evan <39568306+Evanlyboy@users.noreply.github.com>
2023-04-02 01:58:30 -0400

    excellent, the views work

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* get the audio component */
+router.get('/audio', function (req, res, next) {
+  res.render('audio', { title: 'Audio' });
+});
+
+/* get the video component */
+router.get('/video', function (req, res, next) {
+  res.render('video', { title: 'Video' });
+});
+
 module.exports = router;
