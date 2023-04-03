30f60d73d2c477130bc4ea3e036db8932caff8a4
Evan <39568306+Evanlyboy@users.noreply.github.com>
2023-04-02 22:15:33 -0400

    redid the video and tried to implement stuff. need to stop for today.

--- public/JS/main.js
+++ public/JS/main.js
@@ -1,55 +1,146 @@
-let isDyslexic = true;
-let isDark = true;
+    const body = document.getElementsByTagName('body')[0];
+    const video = videojs('video');
 
-const body = document.getElementsByTagName('body')[0];
-const video = document.getElementById("video");
-const seekBar = document.getElementById('video-seek-bar');
+    // const englishAudio = document.getElementById('englishAudio');
+    // const japaneseAudio = document.getElementById('japaneseAudio');
+    const englishAudioButton = document.getElementById('englishAudioButton');
+    const japaneseAudioButton = document.getElementById('japaneseAudioButton');
+    // let currentAudioTrack = null;
 
-function toggleFont() {
-  if (isDyslexic) {
-    body.style.fontFamily = 'OpenDyslexic';
-  } else {
-    body.style.fontFamily = 'Arial';
-  }
-  isDyslexic = !isDyslexic;
-}
+    const englishCaptions = video.addRemoteTextTrack({
+        kind: 'captions',
+        label: 'English',
+        srclang: 'en',
+        default: true,
+        src: 'assets/silentVoiceEnglish.vtt'
+      });
 
-function toggleDarkMode() {
-    if (isDark) {
-      body.style.background = '#2d2d2d';
-      body.style.color = 'white';
+    const japaneseCaptions = video.addRemoteTextTrack({
+        kind: 'captions',
+        label: 'Japanese',
+        srclang: 'jp',
+        default: false,
+        src: 'assets/silentVoiceJapanese.vtt'
+      });
+
+      const englishAudio = {
+        id: 'englishAudio',
+        kind: 'audio',
+        label: 'English',
+        language: 'en',
+        src: 'assets/silentVoiceEnglish.mp3'
+      };
+
+      const japaneseAudio = {
+        id: 'japaneseAudio',
+        kind: 'audio',
+        label: 'Japanese',
+        language: 'jp',
+        src: 'assets/silentVoiceJapanese.mp3'
+      };
+
+      video.audioTracks().addTrack(englishAudio);
+      video.audioTracks().addTrack(japaneseAudio);
+
+      video.on('pause', () => {
+        // Pause the audio tracks as well
+        videoPause();
+      });
+
+      video.on('play', () => {
+        // Pause the audio tracks as well
+        videoPlay();
+      });
+
+    function videoPause(){
+        const currentAudioTrack = video.audioTracks().currentTrack();
+        currentAudioTrack && currentAudioTrack.enabled && currentAudioTrack.pause();
+    }
+
+    function videoPlay(){
+        const currentAudioTrack = video.audioTracks().currentTrack();
+        currentAudioTrack && currentAudioTrack.enabled && currentAudioTrack.play();
+    }
+
+    // Add a click event listener to the button
+    englishAudioButton.addEventListener('click', () => {
+
+        video.audioTracks().setTrack(englishAudio);
+
+    // videoPause();
+    // currentAudioTrack = englishAudio;
+    // currentAudioTrack.currentTime = video.currentTime();
+    // videoPlay();
+
+    // console.log(currentAudioTrack);
+    });
+
+    // Add a click event listener to the button
+    japaneseAudioButton.addEventListener('click', () => {
+        
+        video.audioTracks().setTrack(japaneseAudio);
+        // videoPause();
+        // currentAudioTrack = japaneseAudio;
+        // currentAudioTrack.currentTime = video.currentTime();
+        // videoPlay();
+        // console.log(currentAudioTrack);
+    });
+
+
+
+    // Set up toggle functions for font and dark mode
+    var isDyslexic = false;
+    var isDark = false;
+
+    function toggleFont() {
+    if (isDyslexic) {
+        body.style.fontFamily = 'Arial';
     } else {
-      body.style.background = 'white';
-      body.style.color = '#2d2d2d';
+        body.style.fontFamily = 'OpenDyslexic';
+    }
+    isDyslexic = !isDyslexic;
+    }
+
+    function toggleDarkMode() {
+    if (isDark) {
+        body.style.background = 'white';
+        body.style.color = '#2d2d2d';
+    } else {
+        body.style.background = '#2d2d2d';
+        body.style.color = 'white';
     }
     isDark = !isDark;
-  }
+    }
+
+
+
+// 
+
+
+
+
+// Obsolete
 
 // Play & Pause for video
-video.addEventListener("click", function() {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-});
+// video.addEventListener("click", function() {
+//   if (video.paused) {
+//     video.play();
+//   } else {
+//     video.pause();
+//   }
+// });
+// // Seek bar for video
+// // Update the seek bar as the video plays
+// video.addEventListener('timeupdate', function() {
+//   const percentage = (video.currentTime / video.duration) * 100;
+//   seekBar.value = percentage;
+// });
 
-// Seek bar for video
-// Update the seek bar as the video plays
-video.addEventListener('timeupdate', function() {
-  const percentage = (video.currentTime / video.duration) * 100;
-  seekBar.value = percentage;
-});
-
-// Seek the video when the user drags the seek bar
-seekBar.addEventListener('input', function() {
-  const time = video.duration * (seekBar.value / 100);
-  video.currentTime = time;
-});
-
-
-
-
+// // Seek the video when the user drags the seek bar
+// seekBar.addEventListener('input', function() {
+//   const time = video.duration * (seekBar.value / 100);
+//   video.currentTime = time;
+// });
 
 // Vue Graveyard
 // const app = Vue.createApp({
